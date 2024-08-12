ebb73586a51e808439aa7587fb8ffbc8fce5b2b0
mewadadeepak <63440853+mewadadeepak@users.noreply.github.com>
2024-08-11 23:34:54 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import json
 
-"""""
+""""
 # some JSON:
 x =  '{ "name":"John", "age":30, "city":"New York"}'
 
@@ -35,6 +35,26 @@ print(json.dumps(True))
 print(json.dumps(False))
 print(json.dumps(None))                       '''
 
+'''
+x = {
+  "name": "John",
+  "age": 30,
+  "married": True,
+  "divorced": False,
+  "children": ("Ann","Billy"),
+  "pets": None,
+  "cars": [
+    {"model": "BMW 230", "mpg": 27.5},
+    {"model": "Ford Edge", "mpg": 24.1}
+  ]
+}                         
+
+# convert into JSON:
+y = json.dumps(x)
+
+# the result is a JSON string:
+print(y)                                  '''
+
 import json
 
 x = {
@@ -50,9 +70,6 @@ x = {
   ]
 }
 
-# convert into JSON:
-y = json.dumps(x)
-
-# the result is a JSON string:
-print(y)
+# use four indents to make it easier to read the result:
+print(json.dumps(x, indent=4))
 
